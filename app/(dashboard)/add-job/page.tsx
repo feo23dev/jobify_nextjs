f97535f2d8dcd0eba430df08a5175272937d4c66
efe okyar <f.efe.okyar@gmail.com>
2024-04-23 22:11:08 +0300

    createJobAction completed,started getAllJobsAction

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,10 +1,19 @@
 import CreateJobForm from "../../../components/CreateJobForm";
 
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
 const AddJobPage = () => {
+  const queryClient = new QueryClient();
   return (
-    <div className="px-8">
-      <CreateJobForm></CreateJobForm>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="px-8">
+        <CreateJobForm></CreateJobForm>
+      </div>
+    </HydrationBoundary>
   );
 };
 
--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -6,10 +6,17 @@ import {
   JobMode,
   createAndEditJobSchema,
   CreateAndEditJobType,
+  JobType,
 } from "@/utils/types";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import { CustomFormField, CustomFormSelect } from "./FormComponents";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createJobAction } from "@/utils/actions";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { use } from "react";
 
 function CreateJobForm() {
   // 1. Define your form.
@@ -24,8 +31,31 @@ function CreateJobForm() {
     },
   });
 
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({ description: "There was an error" });
+        return;
+      }
+      toast({ description: "Job created successfully" });
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      queryClient.invalidateQueries({ queryKey: ["stats"] });
+      queryClient.invalidateQueries({ queryKey: ["charts"] });
+
+      router.push(`/jobs/`);
+    },
+    onError: (error) => {
+      toast({ description: "There was an error" });
+    },
+  });
+
   function onSubmit(values: CreateAndEditJobType) {
-    console.log(values);
+    mutate(values);
   }
 
   return (
@@ -62,7 +92,12 @@ function CreateJobForm() {
             items={Object.values(JobMode)}
           />
 
-          <Button type="submit" className="self-end capitalize">
+          <Button
+            type="submit"
+            className="self-end capitalize"
+            disabled={isPending}
+          >
+            {isPending ? "Loading" : "Create Job"}
             create job
           </Button>
         </div>
@@ -71,84 +106,3 @@ function CreateJobForm() {
   );
 }
 export default CreateJobForm;
-
-// "use client";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { Button } from "@/components/ui/button";
-// import { Form } from "@/components/ui/form";
-// import { CustomFormField, CustomFormSelect } from "./FormComponents";
-
-// import {
-//   JobStatus,
-//   JobMode,
-//   createAndEditJobSchema,
-//   CreateAndEditJobType,
-// } from "@/utils/types";
-
-// const CreateJobForm = () => {
-//   const form = useForm<CreateAndEditJobType>({
-//     resolver: zodResolver(createAndEditJobSchema),
-//     defaultValues: {
-//       position: "",
-//       company: "",
-//       location: "",
-//       status: JobStatus.Pending,
-//       mode: JobMode.FullTime,
-//     },
-//   });
-
-//   function onSubmit(values: CreateAndEditJobType) {
-//     console.log(values);
-//   }
-
-//   return (
-//     <Form {...form}>
-//       <form
-//         onSubmit={form.handleSubmit(onSubmit)}
-//         className="bg-muted p-8 rounded"
-//       >
-//         <h2 className="capitalize font-semibold text-4xl mb-6">add job</h2>
-//         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
-//           {/* Position */}
-//           <CustomFormField
-//             name="position"
-//             control={form.control}
-//           ></CustomFormField>
-//           {/* Company */}
-//           <CustomFormField
-//             name="company"
-//             control={form.control}
-//           ></CustomFormField>
-//           {/* Location */}
-//           <CustomFormField
-//             name="location"
-//             control={form.control}
-//           ></CustomFormField>
-
-//           {/* Status */}
-//           <CustomFormSelect
-//             name="status"
-//             control={form.control}
-//             labelText="job status"
-//             items={Object.values(JobStatus)}
-//           />
-
-//           {/* Mode */}
-//           <CustomFormSelect
-//             name="mode"
-//             control={form.control}
-//             labelText="mode status"
-//             items={Object.values(JobMode)}
-//           />
-
-//           <Button type="submit" className="self-end capitalize">
-//             Create Job
-//           </Button>
-//         </div>
-//       </form>
-//     </Form>
-//   );
-// };
-
-// export default CreateJobForm;
